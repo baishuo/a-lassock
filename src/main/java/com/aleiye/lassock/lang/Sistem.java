5291ee8f0b1a7a50c60111b144fd7ceb5b5253fc
ruibing.zhao <ruibing.zhao@aleiye.com>
2016-02-16 17:51:42 +0800

    destoryables

--- src/main/java/com/aleiye/lassock/lang/Sistem.java
+++ src/main/java/com/aleiye/lassock/lang/Sistem.java
@@ -3,13 +3,15 @@ package com.aleiye.lassock.lang;
 import com.aleiye.lassock.util.SigarUtils;
 
 /**
- * Lassock
+ * Lassock 系统信息类
  * 
  * @author ruibing.zhao
  * @since 2016年1月29日
  * @version 1.0
  */
 public class Sistem {
+
+	private Sistem() {}
 
 	public static final String HOST;
 
@@ -21,5 +23,35 @@ public class Sistem {
 		HOST = SigarUtils.getHostsNameBySigar();
 		IP = SigarUtils.getIPBySigar();
 		MAC = SigarUtils.getMacBySigar();
+	}
+
+	/**
+	 * 系统类型
+	 * 
+	 * @author ruibing.zhao
+	 * @since 2016年2月16日
+	 * @version 1.0
+	 */
+	public static enum OSType {
+		WINDOWS, LINUX, UNIX, MAC, OTHER
+	}
+
+	/**
+	 * 系统
+	 * @return
+	 */
+	public static OSType getSystem() {
+		String name = System.getProperty("os.name");
+		if (name.indexOf("Windows") > -1) {
+			return OSType.WINDOWS;
+		} else if (name.indexOf("Linux") > -1) {
+			return OSType.LINUX;
+		} else if (name.indexOf("Mac") > -1) {
+			return OSType.MAC;
+		} else if (name.indexOf("Unix") > -1) {
+			return OSType.UNIX;
+		} else {
+			return OSType.OTHER;
+		}
 	}
 }
