ddba139bfefad3f606a6675ff56426a8f020f2cd
ruibing.zhao <ruibing.zhao@aleiye.com>
2016-02-25 19:20:57 +0800

    liveness

--- src/main/java/com/aleiye/lassock/liveness/LivenessConfiguration.java
+++ src/main/java/com/aleiye/lassock/liveness/LivenessConfiguration.java
@@ -2,6 +2,13 @@ package com.aleiye.lassock.liveness;
 
 import com.aleiye.lassock.live.conf.Context;
 
+/**
+ * Liveness 配置类
+ * 
+ * @author ruibing.zhao
+ * @since 2016年2月25日
+ * @version 1.0
+ */
 public class LivenessConfiguration {
 	private final Context context;
 
@@ -9,6 +16,11 @@ public class LivenessConfiguration {
 		this.context = context;
 	}
 
+	/**
+	 * 获取Liveness 实例
+	 * @return
+	 * @throws Exception
+	 */
 	public Liveness getInstance() throws Exception {
 		Class<?> livenessClass = Class.forName(context.getString("class"));
 		Liveness liveness = (Liveness) livenessClass.newInstance();
