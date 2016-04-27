6401524d3e1ca5fa2518f56f6bf855915b7cde7d
chunyang.lu <chunyang.lu@aleiye.cn>
2016-04-27 11:51:16 +0800

    bat

--- bin/startup.bat
+++ bin/startup.bat
@@ -1,13 +1,14 @@
 @ECHO off
-
 @ECHO start lassock...
 
 setlocal
 SET msg=lassock started.
 
-SET CURRENT_HOME=%cd%/../
+CD ..\
 
+SET CURRENT_HOME=%cd%
 SET class_path=%CLASS_PATH%
+
 
 SET host_name = %COMPUTERNAME%
 
@@ -19,7 +20,7 @@ IF class_path == "" msg="plese install jdk." goto end
 goto start
 
 :start
-start "Lassock" java.exe -cp %CURRENT_HOME%/conf/*;%CURRENT_HOME%/lassock.jar com.aleiye.lassock.Lassock %host_name% >> %CURRENT_HOME%/logs/run.log 2>&1 &
+start "Lassock" java.exe -cp %CURRENT_HOME%\conf\*;%CURRENT_HOME%\lassock.jar com.aleiye.lassock.Lassock %host_name% >> %CURRENT_HOME%/logs/run.log 2>&1 &
 :end
 
 @ECHO %msg%
