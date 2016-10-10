0f9dd42d917e120325160bba286f714ba9fa6e8f
wentao.yu <wentao.yu@aleiye.com>
2016-10-10 11:09:43 +0800

    解决,程序启动出错的bug

--- bin/aleiye-lassock-check.sh
+++ bin/aleiye-lassock-check.sh
@@ -6,14 +6,14 @@ statuechange=${CURRENT_HOME}/statuechange.aleiye
 
 rm -rf ${statuechange}
 
-sh ${CURRENT_HOME}/bin/lassock-daemon.sh
+sh ${CURRENT_HOME}/bin/lassock-daemon.sh -p start
 
 while [ 1 -eq 1 ]
 do
    if [ -f ${statuechange} ]; then
       rm -rf ${statuechange}
-      sh ${CURRENT_HOME}/bin/shutdown.sh
-      sh ${CURRENT_HOME}/bin/lassock-daemon.sh
+      sh ${CURRENT_HOME}/bin/lassock-daemon.sh -p stop
+      sh ${CURRENT_HOME}/bin/lassock-daemon.sh -p start
    fi
    sleep 5
 done
